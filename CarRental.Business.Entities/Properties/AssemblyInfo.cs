6291b6d07fa3ad01917ef6a1596fa7c63f680da9
Khotso Mokhethi <Mokhetkc@hotmail.com>
2020-09-21 12:39:31 +0200

    Initial setup
    Code Clean up and generation.

--- CarRental.Business.Entities/Properties/AssemblyInfo.cs
+++ CarRental.Business.Entities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Building-End-To-End-Multi-Client-SOA")]
+[assembly: AssemblyTitle("CarRental.Business.Entities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Building-End-To-End-Multi-Client-SOA")]
+[assembly: AssemblyProduct("CarRental.Business.Entities")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e31ee01-de63-48a7-bc57-296fe6421486")]
+[assembly: Guid("c19c534b-83c1-417a-a49a-a5a33f374229")]
 
 // Version information for an assembly consists of the following four values:
 //
